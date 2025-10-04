c415f36e44153254ef476a1a7281e744ff4ee20c
Paximoo <odedahmed152@gmail.com>
2025-10-04 19:35:26 +0530

    fetching past years data

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 from flask import Flask, render_template, request, redirect
+from requests import get as req_get
 
 app = Flask(__name__, template_folder='static//templates')
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
   if request.method == 'POST':
-    print(request.form.get('latitude'))
-    print(request.form.get('longitude'))
-    return redirect('/')
+    lat, long, date = request.form.get('latitude'), request.form.get('longitude'), request.form.get('date')
+    date = date[5:]
+    print(lat, long, date)
+    data = []
+    for i in range(2000, 2002):
+      curr_data = req_get(
+        'https://power.larc.nasa.gov/api/temporal/daily/point' +
+        f'?parameters=T2M&community=RE&latitude={lat}&longitude={long}&start={i}{date.replace('-', '')}&end={i}{date.replace('-', '')}&format=JSON').json()
+      
+      data.append(curr_data)
+    
+    print(data)
+    return render_template('results.html')
+
   else:
     return render_template('index.html')
 

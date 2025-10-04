91341d5dc7446209826430f6e9252d543de526b3
Paximoo <odedahmed152@gmail.com>
2025-10-04 18:27:58 +0530

    added map select form

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__, template_folder='static//templates')
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-  return render_template('index.html')
+  if request.method == 'POST':
+    print(request.form.get('latitude'))
+    print(request.form.get('longitude'))
+    return redirect('/')
+  else:
+    return render_template('index.html')
 
 if __name__ == '__main__':
   app.run(port=8080, host='0.0.0.0', debug=True)

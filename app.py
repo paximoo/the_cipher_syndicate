1f44df27851d40320ee2f093105b7dc3881d390d
Paximoo <odedahmed152@gmail.com>
2025-10-05 07:56:58 +0530

    switched to 1 api call and local parsing

--- app.py
+++ app.py
@@ -7,44 +7,51 @@ app = Flask(__name__, template_folder='static//templates')
 DATA = []
 PARAMETERS = ['T2M', 'T2M_MAX', 'T2M_MIN', 'PRECTOTCORR', 'WD10M', 'WS10M', 'PS', 'QV2M']
 LAT, LONG = None, None
+START_YEAR, END_YEAR = 1990, 2025
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
   if request.method == 'POST':
-    LAT, LONG, date = request.form.get('latitude'), request.form.get('longitude'), request.form.get('date')
-    for i in range(2022, 2024):
-      curr_date = str(i) + date[5:].replace('-', '')#{i}{date.replace('-', '')
-      curr_data = {'_': {'_': curr_date[:4]}}
-      try:
-        curr_data.update(req_get(
-          'https://power.larc.nasa.gov/api/temporal/daily/point' +
-          f'?parameters={','.join(PARAMETERS)}&community=RE&latitude={LAT}&longitude={LONG}&start={curr_date}&end={curr_date}&format=JSON')
-          .json()['properties']['parameter'])
-      except Exception as e:
-        print(e)
-       
-      DATA.append(curr_data)
+    LAT, LONG, date = request.form.get('latitude'), float(request.form.get('longitude'))%360, request.form.get('date')
+    print('\n'*4, date, '\n'*4)
+    print(LAT, LONG)
+    RESPONSE = req_get(
+      'https://power.larc.nasa.gov/api/temporal/daily/point' +
+      f'?parameters={','.join(PARAMETERS)}&community=RE&latitude={LAT}&longitude={LONG if LONG <= 180 else 360-LONG}&start={START_YEAR}0101&end={END_YEAR}0101&format=JSON'
+    ).json()
+    print(RESPONSE)
+    RESPONSE = RESPONSE['properties']['parameter']
+
+    DATA = {}
+    for param, recs in RESPONSE.items():
+      DATA[param] = {}
+      for day, value in recs.items():
+        if day[4:] == date.replace('-','')[4:]:
+          DATA[param][day] = value
+
+    print('\n'*4, DATA, '\n'*4)
 
     param_entries = []
     for i in range(len(PARAMETERS)):
       param_entries.append([])
 
-    for r in DATA:
-      for index, field in enumerate(list(r.items())[1:]):
-        param_entries[index] += list(field[1].values())
+    for index, (param, vals) in enumerate(list(DATA.items())):
+      print('VAALSVALSVASL: ', vals.items())
+      for _, value in enumerate(list(vals.items())):
+        param_entries[index] += [value[1]]
 
-    param_avg = [f"{mean(e):.2f}" for e in param_entries]
-    print(param_avg)
     print(param_entries)
+    param_avg = [f"{mean(e):.2f}" for e in param_entries]
     
     return render_template('results.html',
-      data=DATA,
+      param_entries=param_entries,
       params=PARAMETERS,
       param_num=len(PARAMETERS),
       avgs=param_avg,
       date=date,
       lat=LAT,
-      long=LONG
+      long=LONG,
+      start_year=START_YEAR
     )
 
   else:
